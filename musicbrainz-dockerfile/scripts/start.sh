8956bb2294acdd4a898beeaf712569fa0682c0b5
Jeff Sturgis <jesturgis@linkedin.com>
2017-08-23 23:38:39 -0700

    Enable cron only if the ENABLE_REPLICATION env variable is set

--- musicbrainz-dockerfile/scripts/start.sh
+++ musicbrainz-dockerfile/scripts/start.sh
@@ -2,7 +2,10 @@
 
 eval $( perl -Mlocal::lib )
 
-cron -f &
+if [[ $ENABLE_REPLICATION ]]; then
+  cron -f &
+fi
+
 nginx
 /start_mb_renderer.pl
 start_server --port=55901 -- plackup -I lib -s Starlet -E deployment --nproc 10 --pid fcgi.pid
